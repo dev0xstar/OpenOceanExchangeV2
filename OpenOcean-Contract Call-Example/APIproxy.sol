b6d44317e14fde797ff747e796ea51e87e52abee
Kakao-max <angrybird4837@gmail.com>
2024-05-23 07:37:26 -0400

    updated

--- OpenOcean-Contract Call-Example/APIproxy.sol
+++ OpenOcean-Contract Call-Example/APIproxy.sol
@@ -13,70 +13,12 @@ contract APIproxy {
     receive() external payable {}
 
     //Compatible with ETH=>ERC20, ERC20=>ETH
-    function useAPIData(
-        address fromToken, //fromTokenaddress
-        address toToken, //toTokenaddress
-        uint256 fromAmount, //inamout with dicimals
-        address OOApprove, // 0x6352a56caadc4f1e25cd6c75970fa768a3304e64
-        address OOProxy, // 0x6352a56caadc4f1e25cd6c75970fa768a3304e64
-        bytes memory OOApiData // data param from swap_quote API 
-    )
-        external
-        payable
-    {
-        if (fromToken != _ETH_ADDRESS_) {
-            IERC20(fromToken).transferFrom(msg.sender, address(this), fromAmount);
-            _approveMax(fromToken, OOApprove, fromAmount);
-        } else {
-            require(fromAmount == msg.value);
-        }
+    
 
-        (bool success, bytes memory result ) = OOProxy.call{value: fromToken == _ETH_ADDRESS_ ? fromAmount : 0}(OOApiData);
-        require(success, "API_SWAP_FAILED");
+    
 
-        uint256 returnAmount = _balanceOf(toToken, address(this));
+    
 
-        _transfer(toToken, msg.sender, returnAmount);
-    }
-
-
-    function _approveMax(
-        address token,
-        address to,
-        uint256 amount
-    ) internal {
-        uint256 allowance = IERC20(token).allowance(address(this), to);
-        if (allowance < amount) {
-            if (allowance > 0) {
-                IERC20(token).safeApprove(to, 0);
-            }
-            IERC20(token).safeApprove(to, uint256(-1));
-        }
-    }
-
-    function _transfer(
-        address token,
-        address payable to,
-        uint256 amount
-    ) internal {
-        if (amount > 0) {
-            if (token == _ETH_ADDRESS_) {
-                to.transfer(amount);
-            } else {
-                IERC20(token).safeTransfer(to, amount);
-            }
-        }
-    }
-
-    function _balanceOf(
-        address token, 
-        address who
-    ) internal view returns (uint256) {
-        if (token == _ETH_ADDRESS_ ) {
-            return who.balance;
-        } else {
-            return IERC20(token).balanceOf(who);
-        }
-    }
+    
 
 }
