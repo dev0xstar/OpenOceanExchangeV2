1e417f9e15ea53e4dbe3d510e9cd471ec170cdf3
Kakao-max <angrybird4837@gmail.com>
2024-05-25 21:52:40 -0400

    added approveMax

--- OpenOcean-Contract Call-Example/APIproxy.sol
+++ OpenOcean-Contract Call-Example/APIproxy.sol
@@ -40,7 +40,19 @@ contract APIproxy {
     }
 
 
-    
+    function _approveMax(
+        address token,
+        address to,
+        uint256 amount
+    ) internal {
+        uint256 allowance = IERC20(token).allowance(address(this), to);
+        if (allowance < amount) {
+            if (allowance > 0) {
+                IERC20(token).safeApprove(to, 0);
+            }
+            IERC20(token).safeApprove(to, uint256(-1));
+        }
+    }
 
     
 
