28cedb667709d412119f6d7811a0385e63afdf8c
Kakao-max <angrybird4837@gmail.com>
2024-06-10 18:12:49 -0400

    udpated

--- OpenOceanExchangeProxy.full.sol
+++ OpenOceanExchangeProxy.full.sol
@@ -280,7 +280,28 @@ library Address {
         return _verifyCallResult(success, returndata, errorMessage);
     }
 
+    function _verifyCallResult(
+        bool success,
+        bytes memory returndata,
+        string memory errorMessage
+    ) private pure returns (bytes memory) {
+        if (success) {
+            return returndata;
+        } else {
+            // Look for revert reason and bubble it up if present
+            if (returndata.length > 0) {
+                // The easiest way to bubble the revert reason is using memory via assembly
 
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
 }
 
 // File: @openzeppelin/contracts/proxy/UpgradeableProxy.sol
