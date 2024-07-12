ec7e8f404ac3d808c734f3118b0360fa7fe60d60
dev0xstar <angrybird4837@gmail.com>
2024-07-12 07:22:30 -0400

    updated

--- contracts/UniswapV2Exchange.sol
+++ contracts/UniswapV2Exchange.sol
@@ -120,42 +120,6 @@ contract UniswapV2Exchange is EthRejector, Permitable {
                     revertWithReason(0x00000011696e76616c6964206d73672e76616c75650000000000000000000000, 0x55) // "invalid msg.value"
                 }
 
-                mstore(emptyPtr, TRANSFER_FROM_CALL_SELECTOR_32)
-                mstore(add(emptyPtr, 0x4), caller())
-                mstore(add(emptyPtr, 0x24), and(rawPair, ADDRESS_MASK))
-                mstore(add(emptyPtr, 0x44), amount)
-                if iszero(call(gas(), srcToken, 0, emptyPtr, 0x64, 0, 0)) {
-                    reRevert()
-                }
-            }
-
-            returnAmount := amount
-
-            for {
-                let i := add(poolsOffset, 0x20)
-            } lt(i, poolsEndOffset) {
-                i := add(i, 0x20)
-            } {
-                let nextRawPair := calldataload(i)
-
-                returnAmount := swap(
-                    emptyPtr,
-                    returnAmount,
-                    and(rawPair, ADDRESS_MASK),
-                    and(rawPair, REVERSE_MASK),
-                    shr(NUMERATOR_OFFSET, and(rawPair, NUMERATOR_MASK)),
-                    and(nextRawPair, ADDRESS_MASK)
-                )
-
-                rawPair := nextRawPair
-            }
-
-            switch and(rawPair, WETH_MASK)
-            case 0 {
-                returnAmount := swap(
-                    emptyPtr,
-                    returnAmount,
-                    and(rawPair, ADDRESS_MASK),
                     and(rawPair, REVERSE_MASK),
                     shr(NUMERATOR_OFFSET, and(rawPair, NUMERATOR_MASK)),
                     caller()
